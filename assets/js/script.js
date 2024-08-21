23c8d31add01f54c14e4edb8b57307993e58f960
AlexanderManriquez <alexander.manriquez.s@gmail.com>
2024-08-21 00:06:33 -0400

    upadating functions of the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
 // = 0 piedra, 1 papel, 2 tijera
+//***Pendientes: Agregar reglas a través de un modal(?); mostrar mensaje de felicitacion si el usuario gana, 
+//agregar botón de reinicio, agregar nombre al usuario (?) */
 let cantidadDeJuegos;
 let juegosRestantes;
 let eleccionUsuario;
@@ -11,9 +13,10 @@ function startGame() {
     cantidadDeJuegos = parseInt(document.getElementById("selection").value);
     juegosRestantes = cantidadDeJuegos; // Inicializar el contador de juegos restantes
     document.getElementById("buttons").style.visibility = "visible";
+    document.getElementById("results").style.visibility = "visible";
 }
 
-//Se captura la elección del usuario
+//Se captura la elección del usuario mientras queden juegos restantes
 document.querySelectorAll('.choice').forEach(button => {
     button.addEventListener('click', function() {
         if (juegosRestantes > 0) {
@@ -26,13 +29,15 @@ document.querySelectorAll('.choice').forEach(button => {
     });
 });
 
+// Funcion para que se desarrolle el juego, incluye la generacion de la eleccion de la maquina, mostrar las selecciones de los jugadores,
+// disminuir el contador de juegos restantes, y actualizar la cuenta
 function jugar() {
     eleccionMaquina = Math.floor(Math.random() * 3);
     mostrarElecciones(eleccionUsuario, eleccionMaquina);
     determinarGanador(eleccionUsuario, eleccionMaquina);
     juegosRestantes--;
     actualizarCuenta();
-    document.getElementById("results").style.visibility = "visible";
+
 }
 
 // Función para convertir las elecciones a texto
@@ -54,9 +59,15 @@ function determinarGanador(eleccionUsuario, eleccionMaquina) {
     if (eleccionUsuario == 0 && eleccionMaquina == 1) {
         resultado = "Gana la máquina";
         victoriasMaquina++;
+    } else if (eleccionUsuario == 1 && eleccionMaquina == 0) {
+        resultado = "Gana el usuario";
+        victoriasUsuario++;    
     } else if (eleccionUsuario == 0 && eleccionMaquina == 2) {
         resultado = "Gana el usuario";
         victoriasUsuario++;
+    } else if (eleccionUsuario == 2 && eleccionMaquina == 0) {
+        resultado = "Gana la máquina";
+        victoriasMaquina++;
     } else if (eleccionUsuario == 1 && eleccionMaquina == 2) {
         resultado = "Gana la máquina";
         victoriasMaquina++;
@@ -66,7 +77,6 @@ function determinarGanador(eleccionUsuario, eleccionMaquina) {
     } else {
         resultado = "Es un empate";
     }
-    // document.getElementById("resultadoText").textContent = resultado;
 }
 
 // Función para actualizar la cuenta de victorias
